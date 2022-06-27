7afdaa855dada2010b91e83534f3059ea8df327b
rox <seellk@gmail.com>
2022-06-27 11:23:42 +0800

    Fixed syntax

--- clean_6125_all.sh
+++ clean_6125_all.sh
@@ -2,11 +2,12 @@ START=`date`
 TARGET=RTF6125VW
 ACTION=clean
 echo "$ACTION $TARGET All..."
-fileName="$ACTION_${TARGET}_all_`date +%m%d_%H%M`"
+fileName="${ACTION}_${TARGET}_all_`date +%m%d_%H%M`"
 
-sudo time make ASP_MODEL=$TARGET ASP_PROFILE=SEI $ACTION  &> $fileName
+sudo make ASP_MODEL=$TARGET ASP_PROFILE=SEI $ACTION  &> $fileName
 
 END=`date`
+
 echo ""
 echo ""
 echo ""
