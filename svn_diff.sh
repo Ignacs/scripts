6c01a0090a2bad6a19ce4357d69333569ce2e2cc
rox <seellk@gmail.com>
2022-06-27 11:22:33 +0800

    Add compare mech. - same: print message and ignore - diff: compare with vimdif()

--- svn_diff.sh
+++ svn_diff.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
-echo $#
-if [ $# -gt 1 ] ; then 
+# echo $#
+if [ $# -gt 1 ] ; then
     wpath=$2
-    echo "asdfasdf"
     svn cat $1 > $2/tmp.rhead
-    vimdiff $1 $2/tmp.rhead
+	if [ -n "$(cmp $2/tmp.rhead $1)" ]; then
+		vimdiff $2/tmp.rhead $1
+		echo " Different : $2/tmp.rhead $1"
+	else
+		echo " Same : $2/tmp.rhead $1"
+	fi
 else
-    echo "same"
     svn cat $1 > $1.rhead
-    vimdiff $1 $1.rhead
-fi 
+	if [ -n "$(cmp $1.rhead $1)" ]; then
+		vimdiff $1.rhead $1
+		echo " Different : $1.rhead $1"
+	else
+		echo " Same : $1.rhead $1"
+	fi
+fi
 
