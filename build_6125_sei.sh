b41f3ee26cd26db2891520060e2f97ffabfc3eda
rox <seellk@gmail.com>
2022-06-27 11:05:56 +0800

    - add sync mech - follow SDK build code script - Clear ezmesh whatever

--- build_6125_sei.sh
+++ build_6125_sei.sh
@@ -6,10 +6,27 @@ echo "build $PROFILE on $TARGET..."
 OUTPUT_FILE="build_${TARGET}_${PROFILE}_`date +%m%d_%H%M`"
 LAST_LOG=the_last.log
 
+if [ `hostname` == "pve" ]; then
+	echo "Cannot run in phyical environment"
+	exit 0
+fi
+
 if [ -f done ] ; then
 	echo '[done] is exist, copy first'
 	exit 0
 fi
+
+# sudo rm -f app/gpl/wifi/qcom/qsdk-spf11.4/done
+# EZMESH_BUILD=`find app/gpl/wifi/qcom/qsdk-spf11.4/build_dir -name qca-ezmesh-all_g `
+# # /home/ignacs/lab/RTF6125VW_ezmesh/app/gpl/wifi/qcom/qsdk-spf11.4/build_dir/target-aarch64-poky-linux_glibc/qca-ezmesh-all_g/install/
+# for i in $EZMESH_BUILD
+# do
+# 	echo $i
+# 	sudo rm -rf $i
+# done
+# sudo rm -rf app/gpl/wifi/qcom/qsdk-spf11.4/build_dir/host/stamp
+# find app/gpl/wifi/qcom/qsdk-spf11.4/build_dir/target-aarch64-poky-linux_glibc -name qca-wsplcd-map-g | sudo xargs rm -rf
+# find app/gpl/wifi/qcom/qsdk-spf11.4/build_dir/target-aarch64-poky-linux_glibc -name qca-whc-lbd-g | sudo xargs rm -rf
 
 if [ $# -gt 0 ]; then
     max_jobs="ASP_MAX_JOBS=AUTO"
@@ -20,19 +37,18 @@ if [ $# -gt 0 ]; then
     make ASP_MODEL=$TARGET ASP_PROFILE=${PROFILE} $max_jobs $@ &> $OUTPUT_FILE
 
 else
-	sudo rm -rf app/gpl/wifi/qcom/qsdk-spf11.4/build_dir/host/stamp
-	sudo find app/gpl/wifi/qcom/qsdk-spf11.4/build_dir/target-aarch64-poky-linux_glibc -name qca-wsplcd-map-g | xargs sudo rm -rf
 
-	sudo make ASP_MODEL=$TARGET ASP_PROFILE=${PROFILE} ASP_MAX_JOBS=AUTO build_kernel build_kmodule &> $OUTPUT_FILE &&
-	sudo make ASP_MODEL=$TARGET ASP_PROFILE=${PROFILE} ASP_MAX_JOBS=AUTO -C lib/ gpl_lib public_lib private_lib &> $OUTPUT_FILE &&
-	sudo make ASP_MODEL=$TARGET ASP_PROFILE=${PROFILE} -C app/gpl/ samba iproute2 &> $OUTPUT_FILE &&
-	# sudo make ASP_MODEL=$TARGET ASP_PROFILE=${PROFILE} ASP_MAX_JOBS=AUTO -C app/private/wdwdv3 qcom_related_install qcom_ezmesh_install qcom_related &> $OUTPUT_FILE &&
-	# sudo make ASP_MODEL=$TARGET ASP_PROFILE=${PROFILE} ASP_MAX_JOBS=AUTO -C app/private/wdwdv3 qcom_related &> $OUTPUT_FILE &&
-	# sudo make ASP_MODEL=$TARGET ASP_PROFILE=${PROFILE} ASP_MAX_JOBS=AUTO -C app/private/wdwdv3 qcom_related_install ezmesh_install &> $OUTPUT_FILE &&
-	sudo make ASP_MODEL=$TARGET ASP_PROFILE=${PROFILE} ASP_MAX_JOBS=AUTO -C app/private/wdwdv3 qcom_related_install &> $OUTPUT_FILE &&
-	sudo make ASP_MODEL=$TARGET ASP_PROFILE=${PROFILE} -C app/private/ msg-simulator decodepcap &> $OUTPUT_FILE&&
-	sudo make ASP_MODEL=$TARGET ASP_PROFILE=${PROFILE} -C lib/public/open/ libnl &> $OUTPUT_FILE &&
-	sudo make ASP_MODEL=$TARGET ASP_PROFILE=${PROFILE} ASP_MAX_JOBS=AUTO all &> $OUTPUT_FILE
+	make ASP_MODEL=$TARGET ASP_PROFILE=${PROFILE} ASP_MAX_JOBS=AUTO build_kernel build_kmodule &> $OUTPUT_FILE &&
+	make ASP_MODEL=$TARGET ASP_PROFILE=${PROFILE} ASP_MAX_JOBS=AUTO -C lib/ gpl_lib public_lib private_lib &> $OUTPUT_FILE &&
+	make ASP_MODEL=$TARGET ASP_PROFILE=${PROFILE} ASP_MAX_JOBS=AUTO -C bsp/CA/aarch64-venus/lib/public/ libca-app &> $OUTPUT_FILE &&
+	make ASP_MODEL=$TARGET ASP_PROFILE=${PROFILE} -C lib/public/open/ libnl &> $OUTPUT_FILE &&
+	make ASP_MODEL=$TARGET ASP_PROFILE=${PROFILE} -C app/gpl/ samba iproute2 &> $OUTPUT_FILE &&
+	make ASP_MODEL=$TARGET ASP_PROFILE=${PROFILE} -C app/private/ msg-simulator decodepcap &> $OUTPUT_FILE &&
+	# make ASP_MODEL=$TARGET ASP_PROFILE=${PROFILE} ASP_MAX_JOBS=AUTO -C app/private/wdwdv3 qcom_related_install qcom_ezmesh_install qcom_related &> $OUTPUT_FILE &&
+	# make ASP_MODEL=$TARGET ASP_PROFILE=${PROFILE} ASP_MAX_JOBS=AUTO -C app/private/wdwdv3 qcom_related &> $OUTPUT_FILE &&
+	# make ASP_MODEL=$TARGET ASP_PROFILE=${PROFILE} ASP_MAX_JOBS=AUTO -C app/private/wdwdv3 qcom_related_install ezmesh_install &> $OUTPUT_FILE &&
+	make ASP_MODEL=$TARGET ASP_PROFILE=${PROFILE} ASP_MAX_JOBS=AUTO -C app/private/wdwdv3 qcom_related qcom_related_install &> $OUTPUT_FILE &&
+	make ASP_MODEL=$TARGET ASP_PROFILE=${PROFILE} ASP_MAX_JOBS=AUTO all &> $OUTPUT_FILE
 fi
 
 END=$(date +%s)
@@ -49,7 +65,7 @@ $PWD/local_cp_image_tftp.sh
 
 echo $((END-START)) | awk '{printf "[Build Time] %02d:%02d:%02d\n", $1/3600, ($1/60)%60, $1%60}'
 
-vi $LAST_LOG +/:\ error:
+vi $LAST_LOG +/rror
 
 
 # generating done file,  to prevent build without syncing
