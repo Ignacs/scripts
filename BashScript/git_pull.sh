b49ce9a28bcd8042040de239d5dc1d59c2e7a27d
Rox Wu <rox_wu@arcadyan.com.tw>
2022-11-10 15:46:12 +0800

    Add git pull script to all submodule under current path

---
+++ BashScript/git_pull.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# warning /bin/bash != /bin/sh
+
+root_folder=$PWD
+GIT_REPOs=$(find . -name ".git" -type d)
+GIT_REPO_LIST=$root_folder/git_repo_list
+echo "" > $GIT_REPO_LIST
+for proj in $GIT_REPOs
+do
+	cd $proj/../
+	echo $PWD >> $GIT_REPO_LIST
+	echo "Move to [$PWD], pull..."
+	git pull .
+	cd - >/dev/null
+done
+echo "Output the repo lists in $GIT_REPO_LIST"
+
