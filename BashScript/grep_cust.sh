63e990aad3782cbeef6cee5b1718bf8f3d5d4cd6
Rox Wu <rox_wu@arcadyan.com.tw>
2023-12-25 14:15:19 +0800

    Open by fzf,  preview by cls(optional) `Ctrl+o` to open the matched items

--- BashScript/grep_cust.sh
+++ BashScript/grep_cust.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Try to grep -- $*"
+echo "Try to -- $*"
 
 # e.q.
 ###
@@ -17,13 +17,14 @@ GREP_INC_FN=$(echo '--include=*.'{c,h})
 # INC_FN=$(echo --include=*vimrc)
 
 RG_INC_FN=$(echo -g '*.c' -g '*.h')
+#RG_INC_FN=$(echo -g '*.vim' -g '.vimrc')
 #RG_INC_FN='-g *.c' '-g *.h'
 # working format
 # grep "$*" ./ -RsnI $GREP_INC_FN ./ > grep_cust_list 2>/dev/null
 
 echo "  -- filename argu: $RG_INC_FN"
-#rg  --vimgrep $RG_INC_FN "$*" ./ > grep_cust_list 2>/dev/null
-rg  --vimgrep $RG_INC_FN "$*" ./ > grep_cust_list 2>/dev/null
+#rg  --vimgrep $RG_INC_FN "$*" ./ > grep_cust_list 1>/dev/null
+# rg  --vimgrep $RG_INC_FN "$*" ./ > grep_cust_list 2>/dev/null
 
 # rg  --vimgrep --type $INC_FN -g ./ > grep_cust_list 2>/dev/null
 
@@ -51,4 +52,12 @@ rg  --vimgrep $RG_INC_FN "$*" ./ > grep_cust_list 2>/dev/null
 ### The target also are including that filename doesn't include '.'
 # grep  "$*"  -Rn -I  ./ > grep_cust_list 2>/dev/null
 
-vi grep_cust_list
+# vi grep_cust_list
+# cat grep_cust_list | fzf
+
+
+
+# grep --recursive --line-number --binary-files=without-match "time" . | fzf --delimiter=':' -n 2.. --preview-window '+{2}-/2' --preview 'clp -h {2} {1}' --bind 'ctrl-o:execute(vim +{2} {1} < /dev/tty)'
+rg  --vimgrep $RG_INC_FN "$*" ./ | fzf --delimiter=':' -n 2.. --preview-window '+{2}-/2' --preview 'clp -h {2} {1}' --bind 'ctrl-o:execute(vim +{2} {1} < /dev/tty)'
+
+echo "--- Dump to grep_cust_list!"
